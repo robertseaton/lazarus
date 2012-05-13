96374d623c7c849ee66fa2f4e7227ea40494499c
robert seaton <robb@os-blog.com>
2012-05-13 09:47:48 -0500

    Adds test for memmove() with overlapping regions.

--- tests/string.c
+++ tests/string.c
@@ -56,9 +56,29 @@ ATF_TC_BODY(test_memmove_no_overlap, tc)
           ATF_REQUIRE(y[i] == 0);
 }
 
+ATF_TC(test_memmove_overlap);
+ATF_TC_HEAD(test_memmove_overlap, tc)
+{
+     atf_tc_set_md_var(tc, "descr", "Test memmove() with overlapping regions.");
+}
+ATF_TC_BODY(test_memmove_overlap, tc) 
+{
+     uchar *x = malloc(100);
+
+     int i;
+     for (i = 0; i < 100; i++)
+          x[i] = i;
+
+     memmove(&x[20], &x[0], 30);
+
+     for (i = 0; i < 30; i++)
+          ATF_REQUIRE(x[i + 20] == i);
+}
+
 ATF_TP_ADD_TCS(tp)
 {
      ATF_TP_ADD_TC(tp, test_memset);
      ATF_TP_ADD_TC(tp, test_memset_zero);
      ATF_TP_ADD_TC(tp, test_memmove_no_overlap);
+     ATF_TP_ADD_TC(tp, test_memmove_overlap);
 }
