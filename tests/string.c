24d3d3fbef4062c5b6619ba42fb59569cafaa1ef
robert seaton <robb@os-blog.com>
2012-05-13 09:31:23 -0500

    Adds test case for memset() corner case. Fails when run by atf-run, passes when run manually.

--- tests/string.c
+++ tests/string.c
@@ -19,7 +19,24 @@ ATF_TC_BODY(test_memset, tc)
           ATF_REQUIRE(x[i] == 0);
 }
 
+ATF_TC(test_memset_zero);
+ATF_TC_HEAD(test_memset_zero, tc)
+{
+     atf_tc_set_md_var(tc, "descr", "Ensure memset() handles size 0.");
+}
+ATF_TC_BODY(test_memset_zero, tc)
+{
+     uchar *x = malloc(20);
+     memset(x, 0, 20);
+     memset(x, 1, 0);
+
+     int i;
+     for (i = 0; i < 20; i++)
+          ATF_REQUIRE(x[i] == 0);
+}
+
 ATF_TP_ADD_TCS(tp)
 {
      ATF_TP_ADD_TC(tp, test_memset);
+     ATF_TP_ADD_TC(tp, test_memset_zero);
 }
